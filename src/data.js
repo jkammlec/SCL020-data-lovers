06bf6275a45f14e225a105bf64a4b88c49b15cde
Camila Arredondo Cartagena <camiarredondo@MacBook-Air-de-Camila.local>
2022-04-28 17:17:47 -0400

    inicio de js

--- src/data.js
+++ src/data.js
@@ -1,9 +1,80 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+// export const example = () => {
+//   return 'example';
+// };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+// export const anotherExample = () => {
+//   return 'OMG';
+// };
+
+import data from './data/rickandmorty/rickandmorty.js';
+
+export const rickandmorty = {
+  getGenders: () => {
+    let genders = data.results.map(
+      g => g.gender
+    );
+    let uniqueGenders = [];
+    for(let i=0; i<genders.length; i++){
+      if(!uniqueGenders.includes(genders[i])){
+        uniqueGenders.push(genders[i]);
+      }
+    }
+    return uniqueGenders.sort();
+  },
+
+  getSpecies: (gender) => {
+    let speciesfilter = data.results.filter( //voy a filtrar la informacion que se encuentra en "results" de json en donde.....
+      element => element.gender === gender // si el genero es exactamente igual al genero seleccionado, tome toda la informacion de ese genero
+    );
+    let species = speciesfilter.map(
+      g => g.species
+    );
+
+    let uniqueSpecies = [];
+    for(let i=0; i<species.length; i++){
+      if(!uniqueSpecies.includes(species[i])){
+        uniqueSpecies.push(species[i]);
+      }
+    }
+    return uniqueSpecies.sort();
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+// function ObtenerComunasUnicasLargo(){
+//   //obtener una descripción de forma unica
+//       console.log(arreglo);
+      // let comunas = arreglo.map(
+      //     elemento => elemento.comuna
+      // );
+      // console.log(comunas);
+  //     let comunaunica = [];
+  //     for(let i = 0; i < comunas.length; i++){
+  //         if(!comunaunica.includes(comunas[i])){
+  //             comunaunica.push(comunas[i]);
+  //         }
+  //     }
+  //     console.log(comunaunica);
+  
+  // }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,28 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import { rickandmorty } from './data.js';
 
-console.log(example, data);
+
+let selectGender = document.getElementById('selectgender'); //obtengo el elemento html con id selectgender
+let genders = rickandmorty.getGenders(); //extrae genders del json
+for (let i = 0; i < genders.length; i++) {
+    let opt = document.createElement('option'); //crea elemento options por cada indice
+    opt.value = genders[i];// se le agrega un valor ( value)
+    opt.innerHTML = genders[i]; // agrega el texto de ese valor 
+    selectGender.appendChild(opt); // para agregarlo como hijo..... para que todos los valores que se vayan ingresando queden dentro del camco select
+}
+
+document.getElementById("selectgender").addEventListener('change', () => { //cada vez que cambia el valor del selectgender, ejecuta (change) lo siquiente
+    
+    let selectGender = document.getElementById('selectgender'); //obtengo el elemento html con id selectgender
+    var genderSelected = selectGender.options[selectGender.selectedIndex].value; // obtengo todos los generos, de esos saco el seleccionado y le obtengo el valor
+
+    let selectSpecies = document.getElementById('selectspecies'); 
+    let species = rickandmorty.getSpecies(genderSelected); 
+    for (let i = 0; i < species.length; i++) {
+        let opt = document.createElement('option'); 
+        opt.value = species[i];
+        opt.innerHTML = species[i]; 
+        selectSpecies.appendChild(opt); 
+    }
+
+
+});

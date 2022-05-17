be76b6ca6de8d871ac3cb187d98b187090150b3c
Camila Arredondo Cartagena <camiarredondo@MacBook-Air-de-Camila.local>
2022-05-17 18:07:15 -0400

    test parte 1

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,53 @@
-import { example, anotherExample } from '../src/data.js';
+import { it } from 'eslint/lib/rule-tester/rule-tester';
+import { rickandmorty } from '../src/data.js';
 
-
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+describe('Genders', () => {
+  it(
+    'Cantidad de generos' // Mensaje que aparecera en la prueba
+    , () => {
+    expect(
+      rickandmorty.getGenders().length // Que voy a probar
+      ).toBe(
+        4 // Que resultado espero de esa prueba
+        );
   });
-
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it('El primer resultado es "Female"', () => {
+    expect(rickandmorty.getGenders()[0]).toBe("Female");
   });
 });
 
-
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+describe('Species', () => {
+  it('Cantidad especies si el genero es Male', () => {
+    expect(rickandmorty.getSpecies("Male").length).toBe(11);
   });
+  it('Cantidad especies si el genero es Female', () => {
+    expect(rickandmorty.getSpecies("Female").length).toBe(7);
+  });
+});
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+describe('Status', () => {
+  it('Cantidad de estados si el genero es Famele y la especie es Human', () => {
+    expect(rickandmorty.getStatus("Female", "Human").length).toBe(3);
+  });
+  it('Cantidad de estados si el genero es Female y la especie es Alien', () => {
+    expect(rickandmorty.getStatus("Female", "Alien").length).toBe(2);
+  });
+});
+
+describe('Characters', () => {
+  it('Cantidad  personajes si el estado es Alive, el genero es Famele y la especie es Human', () => {
+    expect(rickandmorty.getCharacters("Female","Human","Alive").length).toBe(33);
+  });
+  it('Cantidad de personajes si el estado es Dead si el genero es Female y la especie es Alien', () => {
+    expect(rickandmorty.getCharacters("Female", "Alien","Dead").length).toBe(6);
+  });
+});
+
+describe('Character', () => {
+  it('El nombre del personaje es Alien Rick si el id es 15', () => {
+    expect(rickandmorty.getCharacter("15")[0].name).toBe("Alien Rick");
+  });
+  it('El nombre del personaje es Bill si el id es 46', () => {
+    expect(rickandmorty.getCharacter("46")[0].name).toBe("Bill");
   });
 });
